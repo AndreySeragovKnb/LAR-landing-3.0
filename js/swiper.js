b16b53f6b64739857f5f64cadb50ccac93a5be03
Andrey Seragov <andrey.seragov@kanbanchi.com>
2022-11-03 14:04:38 +0300

    Верстка 03.11.2022 без адаптива

--- js/swiper.js
+++ js/swiper.js
@@ -1,26 +1,46 @@
-const swiper = new Swiper('.blog__slider', {
-  slidesPerView: "auto",
+// const swiper = new Swiper('.swiper', {
+//   slidesPerView: "auto",
+//   spaceBetween: 32,
+//   breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 1.1,
+//     },
+//     450: {
+//       slidesPerView: 1.5,
+//     },
+//     680: {
+//       slidesPerView: 2,
+//     },
+//     786: {
+//       slidesPerView: 2.5,
+//     },
+//     1100: {
+//       slidesPerView: 3,
+//     },
+//     1440: {
+//       slidesPerView: 3,
+//     },
+//   }
+// });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
   spaceBetween: 32,
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1.1,
-    },
-    450: {
-      slidesPerView: 1.5,
-    },
-    680: {
-      slidesPerView: 2,
-    },
-    786: {
-      slidesPerView: 2.5,
-    },
-    1100: {
-      slidesPerView: 3,
-    },
-    1440: {
-      slidesPerView: 3,
-    },
+  watchOverflow: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   }
 });
 
